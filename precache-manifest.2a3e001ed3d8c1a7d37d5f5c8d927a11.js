679eb3488ce356e6301a6179a2be557a7c443841
nazim <nazimaitali88@gmail.com>
2020-09-09 18:26:28 +0100

    Updates

---
+++ precache-manifest.2a3e001ed3d8c1a7d37d5f5c8d927a11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aadf48227eb24d696b05780638c0284",
+    "url": "/jsx/index.html"
+  },
+  {
+    "revision": "9280ff2fa8f167ab3922",
+    "url": "/jsx/static/css/main.03b72377.chunk.css"
+  },
+  {
+    "revision": "b2399f37af0b78ab895b",
+    "url": "/jsx/static/js/2.8add0e9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jsx/static/js/2.8add0e9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9280ff2fa8f167ab3922",
+    "url": "/jsx/static/js/main.325cf0b3.chunk.js"
+  },
+  {
+    "revision": "88ea0852d98fef82365d",
+    "url": "/jsx/static/js/runtime-main.68ed4df1.js"
+  },
+  {
+    "revision": "8cd4fc9fe0c897cdb54541ff619aae0b",
+    "url": "/jsx/static/media/imageInSrc.8cd4fc9f.png"
+  },
+  {
+    "revision": "4c2c4140fce8d102e9b1f39cec15ae46",
+    "url": "/jsx/static/media/myVideo.4c2c4140.mp4"
+  }
+]);
